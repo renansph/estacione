38589c2fa7070b062541e9909693fc9e0cb7dfd3
Gustavo Alves <color.ui>
2018-03-14 00:20:09 -0300

    Tela de Atualização de Vagas

---
+++ app/src/main/java/com/estacione/estacione/atualiza_vagas.java
@@ -0,0 +1,13 @@
+package com.estacione.estacione;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class atualiza_vagas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atualiza_vagas);
+    }
+}
